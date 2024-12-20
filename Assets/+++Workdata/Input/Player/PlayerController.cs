32b48b33af2174072ccb2f5b3a9e43e51f82c967
Angie05r <angelina.raatz@gmx.de>
2024-12-20 14:36:00 +0100

    a

--- Assets/+++Workdata/Input/Player/PlayerController.cs
+++ Assets/+++Workdata/Input/Player/PlayerController.cs
@@ -7,85 +7,144 @@ using UnityEngine.InputSystem;
 // if riders gives no recommendations, something is wrong
 public class PlayerController : MonoBehaviour
 {
-    public GameInput inputActions;
-    public float movementSpeed = 4;
-    
-    private InputAction moveAction;
+   public GameInput inputActions;
+  
+   #region Private Variables
+  
+   [Header("Movement")]
+   [SerializeField]private float movementSpeed = 4f;
+   [SerializeField]private float jumpPower = 4f;
 
-    private SpriteRenderer sr;
-    
-    public Vector2 moveInput; // vector2 is the variable for x,y 
-   
-    public Rigidbody2D rb; // to let the object move and change things
 
-    public bool isFacingRight = true;
+   [Header("GroundCheck")]
+   [SerializeField] private Vector2 boxSize;
+   [SerializeField] private LayerMask groundLayer;
+  
+   private InputAction moveAction;
+   private InputAction JumpAction;
+   private InputAction interactAction;
 
-    private Animator animator;
-    
-    
 
-    public void Flip() // to activate flip - character looks to left or right, depending with direction it walks
-    {
-        isFacingRight = !isFacingRight;
-        Vector3 localScale = transform.localScale; 
-        
-        localScale.x *= -1;
-        
-        transform.localScale = localScale;
-    }
-    
+   private bool isJumping;
+   private bool canJump;
+  
+   private SpriteRenderer sr;
+ 
+   private Vector2 moveInput; // vector2 is the variable for x,y
+   private Rigidbody2D rb; // to let the object move and change things
+   private bool isFacingRight = true;
+ 
+   private bool isGrounded;
+   [SerializeField]private Vector2 boxxOffset;
 
-    private void Awake() 
-    {
-        rb = gameObject.GetComponent<Rigidbody2D>(); // name of the component in the ()
-        
-        inputActions = new GameInput();
-        moveAction = inputActions.Player.Move; // to know that the player has to  move
 
-        animator = GetComponent<Animator>();
-        
-    }
-    
-    private void OnEnable()
-    {
-        inputActions.Enable(); 
+   private Animator animator;
+  
+   #endregion
+  
+   public static readonly int Hash_MovementValue = Animator.StringToHash("Movement");
 
-        moveAction.performed += Move; //subscribed
-        moveAction.canceled += Move;
-        
-    }
-    
-    private void FixedUpdate()
-    {
-        rb.velocity = new Vector2(moveInput.x * movementSpeed, rb.velocity.y);
 
-        if (moveInput.x > 0 )
-        {
-            transform.rotation = Quaternion.Euler(0,0,0);
-        }
-        else if (moveInput.x < 0)
-        {
-            transform.rotation = Quaternion.Euler(0,180,0);
-        }
-    }
-    private void OnDisable()
-    {
-        inputActions.Disable(); //disabled unsere InputMap, MUSS HIER SEIN)
+   public void Flip() // to activate flip - character looks to left or right, depending with direction it walks
+   {
+       isFacingRight = !isFacingRight;
+       Vector3 localScale = transform.localScale;
+      
+       localScale.x *= -1;
+      
+       transform.localScale = localScale;
+   }
+  
 
-        moveAction.performed -= Move; //unsubscribed
-        moveAction.canceled -= Move;
-        
-    }
-    
-    private void Move(InputAction.CallbackContext ctx) // can be ctx or whatever you want
-    {
-        moveInput = ctx.ReadValue<Vector2>(); 
-    }
 
-    private void Update()
-    {
-        animator.SetFloat("MovementValue" , Mathf.Abs(rb.velocity.x)); // abs so the animation goes in every directrion- it makes the number go from negative to positive
-         
-    }
+   private void Awake()
+   {
+       rb = gameObject.GetComponent<Rigidbody2D>(); // name of the component in the ()
+      
+       inputActions = new GameInput();
+       moveAction = inputActions.Player.Move; // to know that the player has to  move
+       
+       JumpAction = inputActions.Player.Jump;
 
+       animator = GetComponent<Animator>();
+      
+   }
+  
+   private void OnEnable()
+   {
+       inputActions.Enable();
+
+
+       moveAction.performed += Move; //subscribed
+       moveAction.canceled += Move;
+      
+       JumpAction.performed += Jump;
+   }
+  
+   private void FixedUpdate()
+   {
+       CheckGround();
+      
+       rb.velocity = new Vector2(moveInput.x * movementSpeed, rb.velocity.y);
+
+
+       if (moveInput.x > 0 )
+       {
+           transform.rotation = Quaternion.Euler(0,0,0);
+       }
+       else if (moveInput.x < 0)
+       {
+           transform.rotation = Quaternion.Euler(0,180,0);
+       }
+   }
+   private void OnDisable()
+   {
+       inputActions.Disable(); //disabled unsere InputMap, MUSS HIER SEIN)
+
+
+       moveAction.performed -= Move; //unsubscribed
+       moveAction.canceled -= Move;
+       
+       JumpAction.performed -= Jump;
+
+   }
+  
+   void CheckGround()
+   {
+       isGrounded = Physics2D.OverlapBox( boxxOffset , transform.position, 0f,  groundLayer);
+   }
+   private void Move(InputAction.CallbackContext ctx) // can be ctx or whatever you want
+   {
+       moveInput = ctx.ReadValue<Vector2>();
+   }
+
+
+   void UpdateAnimator()
+   {
+       int Hash_GroundValue = 0;
+       animator.SetBool(Hash_GroundValue, isGrounded);
+   }
+
+   public void OnDrawGizmosSelected()
+   {
+       throw new NotImplementedException();
+   }
+
+   private void Jump(InputAction.CallbackContext ctx)
+   {
+       rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+      
+   }
+  
+   private void Update()
+   {
+       animator.SetFloat("MovementValue" , Mathf.Abs(rb.velocity.x)); // abs so the animation goes in every directrion- it makes the number go from negative to positive
+   }
+  
 }
+
+
+
+
+
+
---
+++ Assets/+++Workdata/Input/Player/TriggerEventBehaviour.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEventBehaviour : MonoBehaviour
+{
+    public UnityEvent onTrigger;
+
+    public UnityEvent onTriggerEnter;
+    public UnityEvent onTriggerExit;
+    
+    public CinemachineVirtualCamera zoomCam;
+    public CinemachineVirtualCamera playerCam;
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            print(other.name);
+            //print(other,gameObject.name);  ->längere Version
+
+            zoomCam.Priority = 10;
+            playerCam.Priority = 0;
+           
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+        zoomCam.Priority = 0;
+        playerCam.Priority = 10;
+        }
+    }
+    
+    
+}
